1e9107f63bb2ab7fddf310a3e909e6265ada7832
Riwaj Mainali <riwaj227@gmail.com>
2022-04-29 19:18:18 -0500

    today's commit
    i can display a window now :)
    bugs yet to be fixed

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,29 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-int main(){
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    std::cout<<"esdfas\n";
-    return 0;
+int main() {
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+  GLFWwindow *wd = glfwCreateWindow(800, 600, "OpenGL", NULL, NULL);
+  if (wd == NULL) {
+    std::cout << "Failed to create GLFW wondow" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+    std::cout<<"failed to initialize GLAD"<<std::endl;
+  }
+    glViewport(0, 0, 800, 600);
+
+  while (!glfwWindowShouldClose(wd)) {
+
+    glfwSwapBuffers(wd);
+    glfwPollEvents();
+  }
+ 
+  glfwTerminate(); return 0;
 }
