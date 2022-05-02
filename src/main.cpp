23f726f1c647bb20d5c7d2fc53d32eab99ba2bde
Riwaj Mainali <riwaj227@gmail.com>
2022-05-02 08:23:56 -0500

    added comments and fixed error
    clang-format gave me errors. fix not implemented yet

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,44 @@
-#include <glad/glad.h>
+#include <glad/glad.h>  //glad should always be on top cuz glad uses glfw??
 #include <GLFW/glfw3.h>
-#include <iostream>
 
+#include <iostream>
+void framebuffer_size_callback(GLFWwindow *wd, int width, int height) {
+  glViewport(0, 0, width, height);  // changing size if the size of window changes
+}
 int main() {
   glfwInit();
+  if (!glfwInit()) {
+    return -1;
+  }
+  // glfwWindowHint will give hints on what to expect out of the window ig
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
+  // making  a pointer to a window
   GLFWwindow *wd = glfwCreateWindow(800, 600, "OpenGL", NULL, NULL);
+
   if (wd == NULL) {
     std::cout << "Failed to create GLFW wondow" << std::endl;
     glfwTerminate();
-    return -1;
+    return -3;
   }
-  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
-    std::cout<<"failed to initialize GLAD"<<std::endl;
+  glfwMakeContextCurrent(wd);  // if we dont do this, compiler throws a tantrum
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -2;
   }
-    glViewport(0, 0, 800, 600);
+  glViewport(0, 0, 800, 600);  // telling OpenGL the size of the window
 
-  while (!glfwWindowShouldClose(wd)) {
+  glfwSetFramebufferSizeCallback(
+      wd, framebuffer_size_callback);  // making sure that the window gets resized every frame
 
-    glfwSwapBuffers(wd);
-    glfwPollEvents();
+  while (!glfwWindowShouldClose(wd)) {	//loops every frame
+    glfwSwapBuffers(wd);	//swaps the buffer so that we can work on it and it displays the one we already finished writing to
+    glfwPollEvents();		//checks for events activations like key pressed
   }
- 
-  glfwTerminate(); return 0;
+
+  glfwTerminate();
+  return 0;
 }
